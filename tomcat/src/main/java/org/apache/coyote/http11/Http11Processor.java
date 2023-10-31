3ec16996b1c069a9f4fe7d38dc6899ea857a094d
Brian <bwook9908@gmail.com>
2023-10-31 15:38:42 +0900

    feat: 1. get /index.html 응답하기 구현

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,8 +5,13 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
 
@@ -28,16 +33,40 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            final var responseBody = "Hello world!";
+            InputStreamReader reader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String requestLine = bufferedReader.readLine();
+            ArrayList<String> requestLineList = Arrays.stream(requestLine.split(" ")).collect(Collectors.toCollection(ArrayList::new));
 
-            final var response = String.join("\r\n",
-                    "HTTP/1.1 200 OK ",
-                    "Content-Type: text/html;charset=utf-8 ",
-                    "Content-Length: " + responseBody.getBytes().length + " ",
-                    "",
-                    responseBody);
-
-            outputStream.write(response.getBytes());
+            if (Objects.equals(requestLineList.get(1), "/index.html")) {
+                FileInputStream fileStream = new FileInputStream("tomcat/src/main/resources/static/index.html");
+                InputStreamReader fileReader = new InputStreamReader(fileStream);
+                BufferedReader fileBufferedReader =  new BufferedReader(fileReader);
+                StringBuilder fileContent = new StringBuilder();
+                String line;
+                while ((line = fileBufferedReader.readLine()) != null) {
+                    fileContent.append(line);
+                }
+                fileBufferedReader.close();
+                fileReader.close();
+                fileStream.close();
+                String responseBody = fileContent.toString();
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",responseBody);
+                outputStream.write(response.getBytes());
+            }else {
+                final var responseBody = "Hello world!";
+                final var response = String.join("\r\n",
+                        "HTTP/1.1 200 OK ",
+                        "Content-Type: text/html;charset=utf-8 ",
+                        "Content-Length: " + responseBody.getBytes().length + " ",
+                        "",
+                        responseBody);
+                outputStream.write(response.getBytes());
+            }
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
