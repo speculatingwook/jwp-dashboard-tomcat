85bdff7df3c47bb33a59b854e80fe6975987edf5
Brian <bwook9908@gmail.com>
2023-11-01 11:59:08 +0900

    refactor: test case 통과하게 수정, index뿐만 아니라 다른 리소스도 받아올 수 있게 설계

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -8,9 +8,10 @@ import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.Socket;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Http11Processor implements Runnable, Processor {
@@ -30,46 +31,78 @@ public class Http11Processor implements Runnable, Processor {
 
     @Override
     public void process(final Socket connection) {
+        try{
+            if (isUrlIndex(connection)) {
+                getResourse(connection, getUrl(connection));
+            }else {
+                none(connection);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void none(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
-            InputStreamReader reader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            String requestLine = bufferedReader.readLine();
-            ArrayList<String> requestLineList = Arrays.stream(requestLine.split(" ")).collect(Collectors.toCollection(ArrayList::new));
+            final var responseBody = "Hello world!";
 
-            if (Objects.equals(requestLineList.get(1), "/index.html")) {
-                FileInputStream fileStream = new FileInputStream("tomcat/src/main/resources/static/index.html");
-                InputStreamReader fileReader = new InputStreamReader(fileStream);
-                BufferedReader fileBufferedReader =  new BufferedReader(fileReader);
-                StringBuilder fileContent = new StringBuilder();
-                String line;
-                while ((line = fileBufferedReader.readLine()) != null) {
-                    fileContent.append(line);
-                }
-                fileBufferedReader.close();
-                fileReader.close();
-                fileStream.close();
-                String responseBody = fileContent.toString();
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",responseBody);
-                outputStream.write(response.getBytes());
-            }else {
-                final var responseBody = "Hello world!";
-                final var response = String.join("\r\n",
-                        "HTTP/1.1 200 OK ",
-                        "Content-Type: text/html;charset=utf-8 ",
-                        "Content-Length: " + responseBody.getBytes().length + " ",
-                        "",
-                        responseBody);
-                outputStream.write(response.getBytes());
-            }
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes().length + " ",
+                    "",
+                    responseBody);
+
+            outputStream.write(response.getBytes());
             outputStream.flush();
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+    private void getResourse(final Socket connection, String url) {
+        try (final var inputStream = connection.getInputStream();
+             final var outputStream = connection.getOutputStream()) {
+
+            final URL resource = getClass().getClassLoader().getResource("static"+ url);
+            final var responseBody = new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+
+            final var response = String.join("\r\n",
+                    "HTTP/1.1 200 OK ",
+                    "Content-Type: text/html;charset=utf-8 ",
+                    "Content-Length: " + responseBody.getBytes(StandardCharsets.UTF_8).length + " ",
+                    "",
+                    responseBody);
+            outputStream.write(response.getBytes());
+            outputStream.flush();
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public boolean isUrlIndex(Socket connection) throws IOException {
+        try{
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String requestLine = bufferedReader.readLine();
+            ArrayList<String> requestLineList = Arrays.stream(requestLine.split(" ")).collect(Collectors.toCollection(ArrayList::new));
+            return requestLineList.get(1).equals("/index.html");
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+        return false;
+    }
+    public String getUrl(Socket connection) throws IOException {
+        try{
+            InputStreamReader reader = new InputStreamReader(connection.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String requestLine = bufferedReader.readLine();
+            ArrayList<String> requestLineList = Arrays.stream(requestLine.split(" ")).collect(Collectors.toCollection(ArrayList::new));
+            return requestLineList.get(1);
+        } catch (IOException | UncheckedServletException e) {
+            log.error(e.getMessage(), e);
+        }
+        return "";
     }
 }
