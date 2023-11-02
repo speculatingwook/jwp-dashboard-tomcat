775703a23afc8708c8ad6dc6c0cdb0db44afa69a
seungtaekhong <hsk4991149@naver.com>
2023-11-02 23:43:47 +0900

    feat: HttpRequest에서 queryParam 파싱 및 저장하도록 수정

--- tomcat/src/main/java/org/apache/coyote/http11/httprequest/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httprequest/HttpRequest.java
@@ -12,14 +12,17 @@ public class HttpRequest {
     private final String path;
     private final String version;
     private final Map<String, String> headers;
+    private final Map<String, String> queryParams;
     private final String body;
 
-    private HttpRequest(HttpMethod method, String path, String version, Map<String, String> headers, String body) {
+    private HttpRequest(HttpMethod method, String path, String version, Map<String, String> headers,
+                        Map<String, String> queryParams, String body) {
         this.method = method;
         this.path = path;
         this.version = version;
         this.headers = headers;
         this.body = body;
+        this.queryParams = queryParams;
     }
 
     public static HttpRequest parse(BufferedReader reader) {
@@ -33,6 +36,7 @@ public class HttpRequest {
             String[] requestLines = str.split(requestLineSplitter);
             HttpMethod httpMethod = parseMethod(requestLines[0]);
             String path = parsePath(requestLines[1]);
+            Map<String, String> queryParams = parseParams(requestLines[1]);
             String version = parseVersion(requestLines[2]);
 
             // 2. Request Headers
@@ -48,11 +52,24 @@ public class HttpRequest {
                 body.append(reader.readLine());
                 body.append("\n");
             }
-
-            return new HttpRequest(httpMethod, path, version, headers, body.toString());
+            return new HttpRequest(httpMethod, path, version, headers, queryParams, body.toString());
         } catch (Exception e) {
             throw new RequestParseException();
         }
+    }
+
+    private static Map<String, String> parseParams(String requestLine) {
+        String trim = StringUtils.trim(requestLine);
+        Map<String, String> params = new HashMap<>();
+        String[] split = trim.split("\\?");
+        if (split.length == 2) {
+            String[] paramSplit = split[1].split("&");
+            for (String param : paramSplit) {
+                String[] keyValue = param.split("=");
+                params.put(keyValue[0], keyValue[1]);
+            }
+        }
+        return params;
     }
 
     private static HttpMethod parseMethod(String str) {
@@ -60,7 +77,9 @@ public class HttpRequest {
     }
 
     private static String parsePath(String requestLine) {
-        return StringUtils.trim(requestLine);
+        String trim = StringUtils.trim(requestLine);
+        String[] split = trim.split("\\?");
+        return split[0];
     }
 
     private static String parseVersion(String requestLine) {
