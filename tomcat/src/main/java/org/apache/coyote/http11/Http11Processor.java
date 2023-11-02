cef094ee74c4a28b03c2fcb63247f6f3a8e200d2
seungtaekhong <hsk4991149@naver.com>
2023-11-02 19:49:25 +0900

    feat: create HttpRequest class

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -2,10 +2,13 @@ package org.apache.coyote.http11;
 
 import nextstep.jwp.exception.UncheckedServletException;
 import org.apache.coyote.Processor;
+import org.apache.utill.HttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
@@ -26,7 +29,12 @@ public class Http11Processor implements Runnable, Processor {
     @Override
     public void process(final Socket connection) {
         try (final var inputStream = connection.getInputStream();
-             final var outputStream = connection.getOutputStream()) {
+             final var outputStream = connection.getOutputStream();
+             final var reader = new InputStreamReader(inputStream);
+             final var bufferedReader = new BufferedReader(reader)) {
+
+            HttpRequest httpRequest = HttpRequest.parse(bufferedReader);
+
 
             final var responseBody = "Hello world!";
 
---
+++ tomcat/src/main/java/org/apache/utill/HttpRequest.java
@@ -0,0 +1,89 @@
+package org.apache.utill;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequest {
+    private final HttpMethod method;
+    private final String version;
+    private final Map<String, String> headers;
+    private final String body;
+
+    private HttpRequest(HttpMethod method, String version, Map<String, String> headers, String body) {
+        this.method = method;
+        this.version = version;
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public static HttpRequest parse(BufferedReader reader) throws IOException {
+        String str;
+
+        // 1. Request Line
+        str = reader.readLine();
+        String[] requestLines = str.split(" ");
+        HttpMethod httpMethod = parseMethod(requestLines[0]);
+        String path = parsePath(requestLines[1]);
+        String version = parseVersion(requestLines[2]);
+
+        // 2. Request Headers
+        Map<String, String> headers = new HashMap<>();
+        while (!(str = reader.readLine()).isEmpty()) {
+            String[] header = str.split(":");
+            headers.put(header[0], header[1]);
+        }
+
+        // 3. body
+        StringBuilder body = new StringBuilder();
+        while ((str = reader.readLine()) != null) {
+            body.append(str);
+            body.append("\n");
+        }
+
+        return new HttpRequest(httpMethod, version, headers, body.toString());
+    }
+
+    private static String parseVersion(String requestLine) {
+        String trim = StringUtils.trim(requestLine);
+        String[] split = trim.split("/");
+        return split[1];
+    }
+
+    private static String parsePath(String requestLine) {
+        return StringUtils.trim(requestLine);
+    }
+
+    private static HttpMethod parseMethod(String str) {
+        if (str.startsWith("GET")) {
+            return HttpMethod.GET;
+        }
+        else if (str.startsWith("POST")) {
+            return HttpMethod.POST;
+        }
+        else if (str.startsWith("PUT")) {
+            return HttpMethod.PUT;
+        }
+        else if (str.startsWith("DELETE")) {
+            return HttpMethod.DELETE;
+        }
+        else if (str.startsWith("PATCH")) {
+            return HttpMethod.PATCH;
+        }
+        else if (str.startsWith("OPTIONS")) {
+            return HttpMethod.OPTIONS;
+        }
+        else if (str.startsWith("HEAD")) {
+            return HttpMethod.HEAD;
+        }
+
+        throw new IllegalArgumentException("지원하지 않는 메서드입니다.");
+    }
+
+    enum HttpMethod {
+        GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD
+    }
+}
