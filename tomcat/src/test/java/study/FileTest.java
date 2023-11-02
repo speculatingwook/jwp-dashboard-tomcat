a59bc31811663ddd768ec439eec2ecc9d8566bef
jution113 <jution113@gmail.com>
2023-11-02 15:13:32 +0900

    Feat: FileTest 문제 해결: 1. 디렉토리 내 파일 경로 찾기, 2. 파일의 내용 읽기

--- tomcat/src/test/java/study/FileTest.java
+++ tomcat/src/test/java/study/FileTest.java
@@ -3,6 +3,10 @@ package study;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
@@ -26,7 +30,8 @@ class FileTest {
         final String fileName = "nextstep.txt";
 
         // todo
-        final String actual = "";
+        URL resourceUrl = getClass().getClassLoader().getResource(fileName);
+        final String actual = resourceUrl.getFile();
 
         assertThat(actual).endsWith(fileName);
     }
@@ -36,14 +41,15 @@ class FileTest {
      * File, Files 클래스를 사용하여 파일의 내용을 읽어보자.
      */
     @Test
-    void 파일의_내용을_읽는다() {
+    void 파일의_내용을_읽는다() throws IOException {
         final String fileName = "nextstep.txt";
 
         // todo
-        final Path path = null;
+        URL resourceUrl = getClass().getClassLoader().getResource(fileName);
+        final Path path = new File(resourceUrl.getPath()).toPath();
 
         // todo
-        final List<String> actual = Collections.emptyList();
+        final List<String> actual = Files.readAllLines(path);
 
         assertThat(actual).containsOnly("nextstep");
     }
