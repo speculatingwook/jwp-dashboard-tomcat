fba725923d6ca08a37263fba4bf325ca8a5bd993
seungtaekhong <hsk4991149@naver.com>
2023-11-02 20:55:12 +0900

    feat: HttpStatus class 생성

---
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpStatus.java
@@ -0,0 +1,25 @@
+package org.apache.coyote.http11;
+
+public enum HttpStatus {
+
+    OK(200, "OK"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int value;
+    private final String reasonPhrase;
+
+    HttpStatus(int value, String reasonPhrase) {
+        this.value = value;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+}
