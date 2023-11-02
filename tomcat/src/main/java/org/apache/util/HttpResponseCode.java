342b5d62958f22a28077772af05545f5d0104495
jution113 <jution113@gmail.com>
2023-11-02 21:33:38 +0900

    Feat: HttpResponseCode 응답코드 추가

---
+++ tomcat/src/main/java/org/apache/util/HttpResponseCode.java
@@ -0,0 +1,31 @@
+package org.apache.util;
+
+public enum HttpResponseCode {
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NO_CONTENT(204, "No Content"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error");
+
+    private final int code;
+    private final String reasonPhrase;
+
+    HttpResponseCode(int code, String reasonPhrase) {
+        this.code = code;
+        this.reasonPhrase = reasonPhrase;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getReasonPhrase() {
+        return reasonPhrase;
+    }
+}
