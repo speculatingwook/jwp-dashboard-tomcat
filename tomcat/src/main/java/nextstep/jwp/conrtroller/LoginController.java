98b0c652ccd20db4f20c1fe77a4e126166501e83
jution113 <jution113@gmail.com>
2023-11-07 16:41:59 +0900

    Feat: LoginController 클래스에 필드, 생성자 추가

--- tomcat/src/main/java/nextstep/jwp/conrtroller/LoginController.java
+++ tomcat/src/main/java/nextstep/jwp/conrtroller/LoginController.java
@@ -1,4 +1,14 @@
 package nextstep.jwp.conrtroller;
 
+import nextstep.jwp.service.LoginService;
+
 public class LoginController {
+    private String requestMethod;
+    private String requestUrl;
+    private LoginService loginService = new LoginService();
+
+    public LoginController(String requestMethod, String requestUrl) {
+        this.requestMethod = requestMethod;
+        this.requestUrl = requestUrl;
+    }
 }
