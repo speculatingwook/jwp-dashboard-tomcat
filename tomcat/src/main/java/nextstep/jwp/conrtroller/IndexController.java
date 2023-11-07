56224a42fd0de396fa2150ac71375fe7db114b15
jution113 <jution113@gmail.com>
2023-11-07 21:13:33 +0900

    Feat: IndexController 클래스 생성

---
+++ tomcat/src/main/java/nextstep/jwp/conrtroller/IndexController.java
@@ -0,0 +1,13 @@
+package nextstep.jwp.conrtroller;
+
+public class IndexController {
+    private String requestMethod;
+    private String requestUrl;
+
+    public IndexController(String requestMethod, String requestUrl) {
+        this.requestMethod = requestMethod;
+        this.requestUrl = requestUrl;
+    }
+
+
+}
