20a14f88b73ecbdf9b5209ddb4345703e79fca44
jution113 <jution113@gmail.com>
2023-11-07 15:55:33 +0900

    Feat: LoginService 클래스에 login 메소드 구현

--- tomcat/src/main/java/nextstep/jwp/service/LoginService.java
+++ tomcat/src/main/java/nextstep/jwp/service/LoginService.java
@@ -1,4 +1,20 @@
 package nextstep.jwp.service;
 
+import nextstep.jwp.db.InMemoryUserRepository;
+import nextstep.jwp.model.User;
+
+import java.util.Optional;
+
 public class LoginService {
+    // 로그인 여부 boolean을 반환
+    public boolean login(String account, String password) {
+        User findUser = findUserByAccount(account);
+        return findUser.checkPassword(password);
+    }
+
+    // 계정 존재 시 계정 반환, 부재 시 예외 처리
+    public User findUserByAccount(String account) {
+        Optional<User> optionalUser = InMemoryUserRepository.findByAccount(account);
+        return optionalUser.orElseThrow(() -> new RuntimeException()) ;
+    }
 }
