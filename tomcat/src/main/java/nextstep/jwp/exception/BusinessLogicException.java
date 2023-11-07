0c40950705ccdc367647d1587a080a1e3bbf9f75
jution113 <jution113@gmail.com>
2023-11-07 16:20:10 +0900

    Feat: BusinessLogicException 에외 클래스 생성

---
+++ tomcat/src/main/java/nextstep/jwp/exception/BusinessLogicException.java
@@ -0,0 +1,11 @@
+package nextstep.jwp.exception;
+
+public class UserException extends RuntimeException {
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Exception cause) {
+        super(message, cause);
+    }
+}
--- tomcat/src/main/java/nextstep/jwp/exception/UserException.java
+++
@@ -1,4 +0,0 @@
-package nextstep.jwp.exception;
-
-public class UserException extends RuntimeException{
-}
