c1ba78a3850ff64b59b6b048a7dce57fabd272c9
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-08 12:53:26 +0900

    refactor(HttpRequest): rename HttpRequest class fields

--- tomcat/src/main/java/org/apache/coyote/http11/HttpRequest/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/http11/HttpRequest/HttpRequest.java
@@ -2,14 +2,14 @@ package org.apache.coyote.http11.HttpRequest;
 
 public class HttpRequest {
 
-	private final String httpMethod;
-	private final String uri;
-	private final String httpVersion;
+	private final String requestLine;
+	private final String requestHeader;
+	private final String requestBody;
 
-	private HttpRequest(String httpMethod, String uri, String httpVersion) {
-		this.httpMethod = httpMethod;
-		this.uri = uri;
-		this.httpVersion = httpVersion;
+	private HttpRequest(String requestLine, String requestHeader, String requestBody) {
+		this.requestLine = requestLine;
+		this.requestHeader = requestHeader;
+		this.requestBody = requestBody;
 	}
 
 	public static HttpRequest of(String httpMethod, String uri, String httpVersion) {
