78a676bd37acb229e6974bc08f98fa6ab33c0f6a
jution113 <jution113@gmail.com>
2023-11-09 16:42:30 +0900

    Feat: create ResourceFinder class

--- tomcat/src/main/java/nextstep/jwp/util/ResourceFinder.java
+++ tomcat/src/main/java/nextstep/jwp/util/ResourceFinder.java
@@ -1,4 +1,27 @@
 package nextstep.jwp.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+
 public class ResourceFinder {
+    public String getResource(String url) throws IOException {
+        URL resource = getClass().getClassLoader().getResource("static" + url);
+        return new String(Files.readAllBytes(new File(resource.getFile()).toPath()));
+    }
+
+    public String getFileExtension(String url) {
+        int lastDotIndex = url.lastIndexOf(".");
+        if (lastDotIndex != -1) {
+            return url.substring(lastDotIndex + 1);
+        }
+        return "";
+    }
+
+    public String getContentType(String fileExtension) {
+        String contentType = ContentType.valueOf(fileExtension.toUpperCase()).getContentType();
+        if(contentType != null) return contentType;
+        return "application/octet-stream";
+    }
 }
