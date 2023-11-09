076ab3dfe9781227d9097c54ec45f0fdaa1b352f
jution113 <jution113@gmail.com>
2023-11-09 14:23:51 +0900

    Fix: modify mappingController for extract domain

--- tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
@@ -2,9 +2,9 @@ package org.apache.coyote.http11;
 
 public class ControllerMapper {
     public Object mappingController(String url) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        String domain = url.replace("/", "").split(".")[0];
+        String domain = url.replaceAll("/", "").split("\\.")[0];
 
-        String controllerClassName = url.substring(0, 1).toUpperCase() + domain.substring(1) + "Controller";
+        String controllerClassName = domain.substring(0, 1).toUpperCase() + domain.substring(1) + "Controller";
         Class<?> controllerClass = Class.forName("nextstep.jwp.controller." + controllerClassName);
         return controllerClass.newInstance();
     }
