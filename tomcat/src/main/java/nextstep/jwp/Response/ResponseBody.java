026a25d40a43b6732ffdd6af022576fb6e968851
jution113 <jution113@gmail.com>
2023-11-09 22:01:39 +0900

    Feat: add constructor and getLength()

--- tomcat/src/main/java/nextstep/jwp/Response/ResponseBody.java
+++ tomcat/src/main/java/nextstep/jwp/Response/ResponseBody.java
@@ -1,2 +1,13 @@
-package nextstep.jwp.Response;public class ResponseBody {
+package nextstep.jwp.Response;
+
+public class ResponseBody {
+    private String fileContent;
+
+    public ResponseBody(String fileContent) {
+        this.fileContent = fileContent;
+    }
+
+    public int getLength() {
+        return fileContent.length();
+    }
 }
