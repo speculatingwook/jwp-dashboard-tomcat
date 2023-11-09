2ef80dce6f0e40e4636ec31d53fffb75257fb4af
jution113 <jution113@gmail.com>
2023-11-09 21:57:02 +0900

    Feat: create constructor and toString()

--- tomcat/src/main/java/nextstep/jwp/Response/ResponseHeader.java
+++ tomcat/src/main/java/nextstep/jwp/Response/ResponseHeader.java
@@ -1,2 +1,23 @@
-package nextstep.jwp.Response;public class ResponseHeader {
+package nextstep.jwp.Response;
+
+public class ResponseHeader {
+    private final String HTTP_VERSION = "http/1.1";
+    private String responseCode;
+    private String responseStatus;
+    private String contentType;
+    private int contentLength;
+
+    public ResponseHeader(String responseCode, String responseStatus, String contentType, int contentLength) {
+        this.responseCode = responseCode;
+        this.responseStatus = responseStatus;
+        this.contentType = contentType;
+        this.contentLength = contentLength;
+    }
+
+    @Override
+    public String toString() {
+        return HTTP_VERSION + " " + responseCode + " " + responseStatus + "\r\n" +
+                "Content-Type: " + contentType + "\r\n" +
+                "Content-Length: " + contentLength + " \r\n";
+    }
 }
