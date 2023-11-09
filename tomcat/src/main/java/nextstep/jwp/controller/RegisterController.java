61b1bcc0536fc443fe9b2555aca475926a914334
jution113 <jution113@gmail.com>
2023-11-10 02:09:55 +0900

    Feat: add field

--- tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/RegisterController.java
@@ -1,4 +1,13 @@
 package nextstep.jwp.controller;
 
+import nextstep.jwp.Response.ResponseBody;
+import nextstep.jwp.Response.ResponseHeader;
+import nextstep.jwp.service.LoginService;
+import nextstep.jwp.util.ResourceFinder;
+
 public class RegisterController {
+    private LoginService loginService = new LoginService();
+    private ResourceFinder resourceFinder = new ResourceFinder();
+    private ResponseHeader responseHeader = new ResponseHeader();
+    private ResponseBody responseBody = new ResponseBody();
 }
