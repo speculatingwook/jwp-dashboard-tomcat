d6e826cd1803f0508838f3edcc2ebeba16654eff
jution113 <jution113@gmail.com>
2023-11-09 13:16:22 +0900

    Feat: mappingController() for using reflection

--- tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
@@ -1,4 +1,11 @@
 package org.apache.coyote.http11;
 
 public class ControllerMapper {
+    public Object mappingController(String url) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        String domain = url.replace("/", "").split(".")[0];
+
+        String controllerClassName = url.substring(0, 1).toUpperCase() + domain.substring(1) + "Controller";
+        Class<?> controllerClass = Class.forName("nextstep.jwp.controller." + controllerClassName);
+        return controllerClass.newInstance();
+    }
 }
