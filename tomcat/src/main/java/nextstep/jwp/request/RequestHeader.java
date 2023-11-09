14f30248e08961c44b20f44628c7da0fd0b56004
jution113 <jution113@gmail.com>
2023-11-10 04:40:21 +0900

    Feat: add field and constructor

--- tomcat/src/main/java/nextstep/jwp/request/RequestHeader.java
+++ tomcat/src/main/java/nextstep/jwp/request/RequestHeader.java
@@ -1,2 +1,39 @@
-package nextstep.jwp.request;public class RequestHeader {
+package nextstep.jwp.request;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class RequestHeader {
+    private final String HOST = "http://localhost:8080";
+    private final String HTTP_VERSION = "http/1.1";
+    private String method;
+    private String path;
+    private String contentType;
+    private int contentLength;
+
+    public RequestHeader() {}
+
+    public RequestHeader(String method, String path, String contentType, int contentLength) {
+        this.method = method;
+        this.path = path;
+        this.contentType = contentType;
+        this.contentLength = contentLength;
+    }
+
+    public URL extractUrl() throws MalformedURLException {
+        String urlString = HOST + path;
+        return new URL(urlString);
+    }
+
+    public int getContentLength() {
+        return contentLength;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getMethod() {
+        return method;
+    }
 }
