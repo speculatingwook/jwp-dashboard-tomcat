5d983f40c51c7163ab8c267307d71c39719b9fe4
jution113 <jution113@gmail.com>
2023-11-10 03:24:11 +0900

    Feat: add parseRequestHeader()

--- tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
+++ tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
@@ -1,2 +1,34 @@
-package nextstep.jwp.util;public class ParsingUtil {
+package nextstep.jwp.util;
+
+import nextstep.jwp.request.RequestHeader;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ParsingUtil {
+    public RequestHeader parseRequestHeader(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String requestLine = reader.readLine();
+
+        String[] requestParts = requestLine.split(" ");
+        String method = requestParts[0];
+        String path = requestParts[1];
+        String contentType = "";
+        int contentLength = 0;
+
+        String line;
+        while ((line = reader.readLine()) != null && !line.isEmpty()) {
+            if (line.startsWith("Content-Type: ")) {
+                contentType = line.substring("Content-Type: ".length());
+            } else if (line.startsWith("Content-Length: ")) {
+                contentLength = Integer.parseInt(line.substring("Content-Length: ".length()));
+            }
+        }
+
+        return new RequestHeader(method, path, contentType, contentLength);
+    }
+
+
 }
