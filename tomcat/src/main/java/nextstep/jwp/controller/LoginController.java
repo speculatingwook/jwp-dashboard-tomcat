34124de356eefe834b94ce86565547d01c076045
jution113 <jution113@gmail.com>
2023-11-09 15:01:58 +0900

    Refactor: remove parameter constructor

--- tomcat/src/main/java/nextstep/jwp/controller/LoginController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/LoginController.java
@@ -1,26 +1,8 @@
-package nextstep.jwp.conrtroller;
+package nextstep.jwp.controller;
 
 import nextstep.jwp.service.LoginService;
-import org.apache.coyote.http11.Response;
 
 public class LoginController {
-    private String requestMethod;
-    private String requestUrl;
     private LoginService loginService = new LoginService();
 
-    public LoginController(String requestMethod, String requestUrl) {
-        this.requestMethod = requestMethod;
-        this.requestUrl = requestUrl;
-    }
-
-    public Response generateResponse() {
-        switch (requestMethod) {
-            case "POST":
-                break;
-            case "GET":
-                break;
-            default:
-                break;
-        }
-    }
 }
