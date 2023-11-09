708dfce1baf045d076b2f2ba7f4eda7f37bc22cb
jution113 <jution113@gmail.com>
2023-11-09 09:22:57 +0900

    Feat: add parsingHttpRequestMessage

--- tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
+++ tomcat/src/main/java/org/apache/coyote/http11/Http11Processor.java
@@ -5,7 +5,10 @@ import org.apache.coyote.Processor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
 
 public class Http11Processor implements Runnable, Processor {
@@ -13,6 +16,8 @@ public class Http11Processor implements Runnable, Processor {
     private static final Logger log = LoggerFactory.getLogger(Http11Processor.class);
 
     private final Socket connection;
+
+    private final ControllerMapper controllerMapper = new ControllerMapper();
 
     public Http11Processor(final Socket connection) {
         this.connection = connection;
@@ -28,6 +33,11 @@ public class Http11Processor implements Runnable, Processor {
         try (final var inputStream = connection.getInputStream();
              final var outputStream = connection.getOutputStream()) {
 
+            // HTTP 요청 메시지 파싱
+            String requestLine = parsingHttpRequestMessage(inputStream);
+
+
+
             final var responseBody = "Hello world!";
 
             final var response = String.join("\r\n",
@@ -42,5 +52,10 @@ public class Http11Processor implements Runnable, Processor {
         } catch (IOException | UncheckedServletException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    private String parsingHttpRequestMessage(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        return reader.readLine();
     }
 }
