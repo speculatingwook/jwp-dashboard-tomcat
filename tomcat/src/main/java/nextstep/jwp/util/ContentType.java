8c49595fc052e4f04417dd60160fa532200206cc
jution113 <jution113@gmail.com>
2023-11-09 16:39:42 +0900

    Feat: create ContentType enum

--- tomcat/src/main/java/nextstep/jwp/util/ContentType.java
+++ tomcat/src/main/java/nextstep/jwp/util/ContentType.java
@@ -1,4 +1,27 @@
 package nextstep.jwp.util;
 
 public enum ContentType {
+    HTML("html", "text/html;charset=utf-8"),
+    JS("js", "application/javascript"),
+    CSS("css", "text/css"),
+    JPG("jpg", "image/jpeg"),
+    JPEG("jpeg", "image/jpeg"),
+    PNG("png", "image/png"),
+    GIF("gif", "image/gif");
+
+    private final String extension;
+    private final String contentType;
+
+    ContentType(String extension, String contentType) {
+        this.extension = extension;
+        this.contentType = contentType;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
 }
