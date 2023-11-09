5c90ba4a0b58a79e35caab5f8b912946199f6089
jution113 <jution113@gmail.com>
2023-11-10 04:39:51 +0900

    Feat: add field and constructor

--- tomcat/src/main/java/nextstep/jwp/request/Request.java
+++ tomcat/src/main/java/nextstep/jwp/request/Request.java
@@ -1,2 +1,19 @@
-package nextstep.jwp.request;public class Request {
+package nextstep.jwp.request;
+
+public class Request {
+    private RequestHeader requestHeader;
+    private RequestBody requestBody;
+
+    public Request(RequestHeader requestHeader, RequestBody requestBody) {
+        this.requestHeader = requestHeader;
+        this.requestBody = requestBody;
+    }
+
+    public RequestHeader getRequestHeader() {
+        return requestHeader;
+    }
+
+    public RequestBody getRequestBody() {
+        return requestBody;
+    }
 }
