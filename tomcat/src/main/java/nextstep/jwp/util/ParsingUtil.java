a7b6a656f7364fd6bb6c265bb0ce2d121d671ad3
jution113 <jution113@gmail.com>
2023-11-10 17:26:00 +0900

    Remove: delete parseRequestHeader()

--- tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
+++ tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
@@ -37,25 +37,4 @@ public class ParsingUtil {
 
         return new Request(requestHeader, requestBody);
     }
-
-    public RequestHeader parseRequestHeader(BufferedReader reader) throws IOException {
-        String requestLine = reader.readLine();
-
-        String[] requestParts = requestLine.split(" ");
-        String method = requestParts[0];
-        String path = requestParts[1];
-        String contentType = "";
-        int contentLength = 0;
-
-        String line;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            if (line.startsWith("Content-Type: ")) {
-                contentType = line.substring("Content-Type: ".length());
-            } else if (line.startsWith("Content-Length: ")) {
-                contentLength = Integer.parseInt(line.substring("Content-Length: ".length()));
-            }
-        }
-
-        return new RequestHeader(method, path, contentType, contentLength);
-    }
 }
