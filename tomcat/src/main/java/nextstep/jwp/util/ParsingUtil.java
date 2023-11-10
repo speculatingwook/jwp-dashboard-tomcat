ce0a5c0007fc32b777a58495d197682efcf8df62
jution113 <jution113@gmail.com>
2023-11-10 17:25:22 +0900

    Remove: delete parseRequestBody()

--- tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
+++ tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
@@ -58,22 +58,4 @@ public class ParsingUtil {
 
         return new RequestHeader(method, path, contentType, contentLength);
     }
-
-    public RequestBody parseRequestBody(BufferedReader reader, RequestHeader requestHeader) throws IOException {
-        int contentLength = requestHeader.getContentLength();
-        if (contentLength > 0) {
-            // 헤더를 모두 읽어옴
-            String line;
-            while ((line = reader.readLine()) != null && !line.isEmpty()) { }
-
-            // 나머지를 바디로 처리
-            StringBuilder bodyBuilder = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                bodyBuilder.append(line).append("\r\n");
-            }
-
-            return new RequestBody(bodyBuilder.toString());
-        }
-        return new RequestBody("");
-    }
 }
