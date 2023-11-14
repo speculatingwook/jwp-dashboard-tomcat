db09525bceebaba0d2ad2ee795e85c6787daf7fe
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-14 16:21:47 +0900

    refactor(Controller): remove `Controller` class

--- tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
+++
@@ -1,8 +0,0 @@
-package org.apache.coyote.http11.controller;
-
-import org.apache.coyote.http11.HttpRequest.HttpRequest;
-import org.apache.coyote.http11.HttpResponse.HttpResponse;
-
-public interface Controller {
-	HttpResponse handleRequest(HttpRequest httpRequest);
-}
