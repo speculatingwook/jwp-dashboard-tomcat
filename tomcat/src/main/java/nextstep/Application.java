c504091013dc4e836be2e82707edc4fd678a60d5
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-15 12:28:01 +0900

    feat(Application): add `ControllerScanner` instance to scan `@RequestMapping` annotated methods

--- tomcat/src/main/java/nextstep/Application.java
+++ tomcat/src/main/java/nextstep/Application.java
@@ -1,6 +1,7 @@
 package nextstep;
 
 import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.common.ControllerScanner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +11,13 @@ public class Application {
 
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-    public static void main(String[] args) {
-        log.info("web server start.");
-        final var tomcat = new Tomcat();
-        tomcat.start();
-    }
+	public static void main(String[] args) {
+		log.info("web server start.");
+
+		ControllerScanner controllerScanner = new ControllerScanner();
+		controllerScanner.start(BASE_PACKAGE);
+
+		final var tomcat = new Tomcat();
+		tomcat.start();
+	}
 }
