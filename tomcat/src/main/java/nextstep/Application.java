dde757ef3bccf557b039a1ff13ee0ce75768dc9d
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-15 12:26:59 +0900

    refactor(Application): replace low readability code with constant

--- tomcat/src/main/java/nextstep/Application.java
+++ tomcat/src/main/java/nextstep/Application.java
@@ -6,7 +6,9 @@ import org.slf4j.LoggerFactory;
 
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+	private static final String BASE_PACKAGE = "org.apache.coyote.http11.controller";
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
         log.info("web server start.");
