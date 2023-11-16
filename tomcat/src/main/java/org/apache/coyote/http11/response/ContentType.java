55c884bf1fc5721d9e212c05cc4290e4132d1da4
jution113 <jution113@gmail.com>
2023-11-17 02:05:01 +0900

    Feat: create ContentType enum

---
+++ tomcat/src/main/java/org/apache/coyote/http11/response/ContentType.java
@@ -0,0 +1,27 @@
+package org.apache.coyote.http11.response;
+
+public enum ContentType {
+    HTML("html", "text/html;charset=utf-8"),
+    JS("js", "application/javascript"),
+    CSS("css", "text/css"),
+    JPG("jpg", "image/jpeg"),
+    JPEG("jpeg", "image/jpeg"),
+    PNG("png", "image/png"),
+    GIF("gif", "image/gif");
+
+    private final String extension;
+    private final String contentType;
+
+    ContentType(String extension, String contentType) {
+        this.extension = extension;
+        this.contentType = contentType;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+}
