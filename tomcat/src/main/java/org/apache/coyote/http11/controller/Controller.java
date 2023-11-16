7d1929c26f8125fac1e9855b74ce495dba61df43
jution113 <jution113@gmail.com>
2023-11-17 02:06:37 +0900

    Refactor: modify return type of handleRequest() in Controller interface

--- tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
@@ -4,5 +4,5 @@ import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
 public interface Controller {
-    void handleRequest(HttpRequest httpRequest, HttpResponse httpResponse);
+    HttpResponse handleRequest(HttpRequest httpRequest, HttpResponse httpResponse);
 }
