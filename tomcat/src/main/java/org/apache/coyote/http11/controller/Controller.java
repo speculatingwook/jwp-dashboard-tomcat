65991e0d228610084ad4f003df31c3ae1d3ef49e
jution113 <jution113@gmail.com>
2023-11-17 00:44:11 +0900

    Refactor: modify parameter of handleRequest() in Controller interface

--- tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
@@ -1,7 +1,8 @@
 package org.apache.coyote.http11.controller;
 
 import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
 
 public interface Controller {
-    void handleRequest(HttpRequest httpRequest);
+    void handleRequest(HttpRequest httpRequest, HttpResponse httpResponse);
 }
