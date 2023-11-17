ac20d432e66e587f495639272b716422edb994ca
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-17 18:31:08 +0900

    feat(RequestHandler): add static factory method

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
@@ -1,12 +1,27 @@
 package org.apache.coyote.http11.handler;
 
+import java.lang.reflect.Method;
 import java.util.Map;
+
 import org.apache.coyote.http11.httprequest.HttpRequest;
 import org.apache.coyote.http11.httpresponse.HttpResponse;
+import org.apache.coyote.http11.scanner.MethodScanner;
+
 public class RequestHandler {
 
 	private HttpRequest httpRequest;
 	private HttpResponse httpResponse;
 	private Map<String, Method> uriToMethodMap;
+
+	private RequestHandler(HttpRequest httpRequest, HttpResponse httpResponse) {
+		this.httpRequest = httpRequest;
+		this.httpResponse = httpResponse;
+		this.uriToMethodMap = MethodScanner.getInstance().getUriToMethodMap();
+	}
+
+	public static RequestHandler of(HttpRequest httpRequest, HttpResponse httpResponse) {
+		return new RequestHandler(httpRequest, httpResponse);
+	}
+
 
 }
