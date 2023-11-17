3df3a330ff864a679dad370de4d794ab8ae50e32
jution113 <jution113@gmail.com>
2023-11-17 15:30:31 +0900

    Feat: add IoException to handleRequest() in Controller interface

--- tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/Controller.java
@@ -3,6 +3,8 @@ package org.apache.coyote.http11.controller;
 import org.apache.coyote.http11.request.HttpRequest;
 import org.apache.coyote.http11.response.HttpResponse;
 
+import java.io.IOException;
+
 public interface Controller {
-    HttpResponse handleRequest(HttpRequest httpRequest, HttpResponse httpResponse);
+    HttpResponse handleRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException;
 }
