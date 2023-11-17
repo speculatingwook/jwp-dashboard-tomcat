0b83475f32bf19adbf98f46f2057d31d606b64a1
jution113 <jution113@gmail.com>
2023-11-17 15:33:59 +0900

    Refactor: modify constructor and processRequest() in FrontController class

--- tomcat/src/main/java/org/apache/coyote/http11/controller/FrontController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/FrontController.java
@@ -1,30 +1,52 @@
 package org.apache.coyote.http11.controller;
 
 import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.response.HttpResponse;
+import org.apache.coyote.http11.response.HttpStatusCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class FrontController {
 
     private static final Logger log = LoggerFactory.getLogger(FrontController.class);
-    private final Map<String, Controller> controllerMap;
+    private final Map<String, Controller> controllers;
 
     public FrontController() {
-        controllerMap = new HashMap<>();
-        controllerMap.put("/index.html", new IndexController());
+        controllers = new HashMap<>();
+        controllers.put("/index", new IndexController());
+        controllers.put("/login", new LoginController());
+        controllers.put("/register", new RegisterController());
+        controllers.put("/assets", new StaticResourceController());
+        controllers.put("/js", new StaticResourceController());
+        controllers.put("/css", new StaticResourceController());
     }
 
-    public void processRequest(HttpRequest httpRequest) {
+    public HttpResponse processRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
         String path = httpRequest.getPath();
-        Controller controller = controllerMap.get(path);
+        Controller controller = null;
+
+        for (String key : controllers.keySet()) {
+            if (path.startsWith(key)) {
+                controller = controllers.get(key);
+            }
+        }
 
         if (controller != null) {
-            controller.handleRequest(httpRequest);
+            httpResponse = controller.handleRequest(httpRequest, httpResponse);
         } else {
             log.warn("No controller found for path: {}", path);
+            httpResponse = new HttpResponse()
+                    .statusCode(HttpStatusCode.NOT_FOUND.getCode())
+                    .statusMessage(HttpStatusCode.NOT_FOUND.getMessage())
+                    .addHeader("Content-Type", "text/html;charset=utf-8")
+                    .addHeader("Content-Length", String.valueOf(0))
+                    .body("<html><body><h1>Hello world!</h1></body></html>")
+                    .build();
         }
+        return httpResponse;
     }
 }
