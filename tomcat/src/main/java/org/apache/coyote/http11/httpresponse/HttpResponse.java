4caf248b592a96629f205a9de57a0e5a20c3d489
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-17 18:27:53 +0900

    feat(HttpResponse): add fields

--- tomcat/src/main/java/org/apache/coyote/http11/httpresponse/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httpresponse/HttpResponse.java
@@ -1,8 +1,17 @@
 package org.apache.coyote.http11.httpresponse;
 
+import java.util.Map;
+
+import org.apache.coyote.http11.common.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
 public class HttpResponse {
+
+	private String httpVersion;
+	private HttpStatus httpStatus;
+	private Map<String, String> responseHeaders;
+	private String responseBody = null;
 
 }
