ee06c5d8b50a7bb1abcce8277be45be7aff51c57
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-17 18:30:55 +0900

    feat(RequestHandler): add fields

--- tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
+++ tomcat/src/main/java/org/apache/coyote/http11/handler/RequestHandler.java
@@ -1,5 +1,12 @@
 package org.apache.coyote.http11.handler;
 
+import java.util.Map;
+import org.apache.coyote.http11.httprequest.HttpRequest;
+import org.apache.coyote.http11.httpresponse.HttpResponse;
 public class RequestHandler {
+
+	private HttpRequest httpRequest;
+	private HttpResponse httpResponse;
+	private Map<String, Method> uriToMethodMap;
 
 }
