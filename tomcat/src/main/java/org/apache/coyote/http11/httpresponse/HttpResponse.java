7b14007ed700236083a3780af6c9b72c50c31506
GOMUNGNAM <rhrjsgh97@gmail.com>
2023-11-19 00:50:08 +0900

    refactor(HttpResponse): remove `@Builder` lombok annotation

--- tomcat/src/main/java/org/apache/coyote/http11/httpresponse/HttpResponse.java
+++ tomcat/src/main/java/org/apache/coyote/http11/httpresponse/HttpResponse.java
@@ -4,11 +4,9 @@ import java.util.Map;
 
 import org.apache.coyote.http11.common.HttpStatus;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class HttpResponse {
 
 	private String httpVersion;
