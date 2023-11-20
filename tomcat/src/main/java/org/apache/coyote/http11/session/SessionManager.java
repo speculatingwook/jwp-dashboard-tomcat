946d4f65c7ca8836f5bffd312dae607d2e713638
jution113 <jution113@gmail.com>
2023-11-20 16:38:48 +0900

    Refactor: rename SessionUtil to SessionManager

--- tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
+++ tomcat/src/main/java/org/apache/coyote/http11/session/SessionManager.java
@@ -23,4 +23,8 @@ public class SessionUtil {
     public void remove(Session session) {
         SESSIONS.remove(session.getSessionId());
     }
+
+    private Session verifySession(String sessionId) {
+
+    }
 }
