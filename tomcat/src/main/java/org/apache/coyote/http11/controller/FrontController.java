0a95878b95e45ecf2421cce1d5be7988b96ca0cf
jution113 <jution113@gmail.com>
2023-11-20 16:55:00 +0900

    Refactor: modify handleRequest() in FrontController class

--- tomcat/src/main/java/org/apache/coyote/http11/controller/FrontController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/FrontController.java
@@ -1,18 +1,14 @@
 package org.apache.coyote.http11.controller;
 
 import org.apache.coyote.http11.request.HttpRequest;
+import org.apache.coyote.http11.request.HttpRequestHeader;
 import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.HttpStatusCode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FrontController {
-
-    private static final Logger log = LoggerFactory.getLogger(FrontController.class);
+public class FrontController implements Controller{
     private final Map<String, Controller> controllers;
 
     public FrontController() {
@@ -21,8 +17,11 @@ public class FrontController {
         controllers.put("/register", new RegisterController());
     }
 
-    public HttpResponse processRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        String path = httpRequest.getPath();
+    @Override
+    public HttpResponse handleRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
+        HttpRequestHeader httpRequestHeader = httpRequest.getHttpRequestHeader();
+
+        String path = httpRequestHeader.getPath();
         Controller controller = null;
 
         for (String key : controllers.keySet()) {
