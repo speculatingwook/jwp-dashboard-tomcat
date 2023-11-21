a22ccfbcd778c38343e86afe3dde8a0b6a88f7d5
Han Ju Kim <hjkim2902@gmail.com>
2023-11-21 18:52:22 +0900

    feat : create RequestBody Class

--- tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
@@ -1,117 +1,67 @@
 package org.apache.coyote.request;
 
+import org.apache.coyote.session.Session;
+
 import javax.naming.directory.NoSuchAttributeException;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.net.URLDecoder;
-import java.util.Arrays;
 
 public class HttpRequest {
-    private RequestParam requestParam;
-    private String path;
-    private String httpMethod;
+    private HttpRequestBody requestBody;
+    private HttpRequestLine httpRequestLine;
     private HttpRequestHeader header;
 
-    private Cookie cookie;
+    private RequestParam requestParam;
+    private Session session;
 
-    public HttpRequest(Reader reader) throws NoSuchAttributeException, IOException {
-        requestParam = new RequestParam();
-        header = new HttpRequestHeader();
 
-        //첫줄은 HTTP 메소드와 URI
-        BufferedReader br = (BufferedReader) reader;
-        parseRequestLine(br);
-        parseRequestHeader(br);
-        parseRequestBody(br);
+    public static HttpRequest parse(BufferedReader br) throws NoSuchAttributeException, IOException {
+        HttpRequestLine requestLine = HttpRequestLine.parse(br);
+        HttpRequestHeader header = HttpRequestHeader.parse(br);
+        HttpRequestBody body = HttpRequestBody.parseBody(br, header.getHeader("Content-Length"));
+        RequestParam params = RequestParam.from(requestLine,body);
+
+        return new HttpRequest(requestLine, header, body, params);
+    }
+
+    private HttpRequest(HttpRequestLine httpRequestLine, HttpRequestHeader header, HttpRequestBody requestBody, RequestParam param) {
+        this.httpRequestLine = httpRequestLine;
+        this.header = header;
+        this.requestBody = requestBody;
+        this.requestParam = param;
     }
 
     public String getHttpMethod() {
-        return httpMethod;
+        return httpRequestLine.getHttpMethod();
     }
 
     public String getPath() {
-        return path;
+        return httpRequestLine.getPath();
     }
 
-    public RequestParam getRequestParam() {
-        return requestParam;
+    public Session getSession() {
+        if (this.session == null) {
+            this.session = new Session();
+        }
+        return session;
     }
+
+    public Session getSession(boolean create) {
+        if (create && this.session == null) {
+            this.session = new Session();
+        }
+        return session;
+    }
+
+    public HttpRequestBody getRequestBody() {
+        return requestBody;
+    }
+
+    public String getParameter(String key) {
+        return requestParam.getParameter(key);
+    }
+
     public String getCookie() {
         return header.getHeader("Cookie");
     }
-
-    public Object getAttribute(String key) {
-        return requestParam.get(key);
-    }
-
-    private void parseRequestLine(BufferedReader br) throws IOException, NoSuchAttributeException {
-        //첫줄 Method & URI parsing
-        String firstLine = br.readLine();
-        if (firstLine == null)
-            throw new NoSuchAttributeException("요청이 잘못 들어옴");
-
-        System.out.println("firstLine = " + firstLine);
-
-        String[] methodPathSplit = firstLine.split(" ");
-        this.httpMethod = methodPathSplit[0];
-        this.path = methodPathSplit[1];
-        System.out.println("path = " + path);
-        System.out.println("httpMethod = " + httpMethod);
-        //parse QueryString and uri
-        if (methodPathSplit[1].contains("?")) {
-            String[] uriQueryString = methodPathSplit[1].split("\\?");
-            this.path = uriQueryString[0];
-
-            //parse QueryString Key & Value
-            parseQueryString(uriQueryString[1]);
-        }
-    }
-
-    private void parseQueryString(String queryString) {
-        String[] queryStringArray = queryString.split("&");
-        Arrays.stream(queryStringArray)
-                .forEach(keyVal -> {
-                    String[] keyValSplit = keyVal.split("=");
-                    requestParam.put(keyValSplit[0], keyValSplit[1]);
-                });
-    }
-
-    private void parseRequestHeader(BufferedReader br) throws IOException {
-        String header = "";
-        while (!(header = br.readLine()).isEmpty()) {
-            System.out.println("header = " + header);
-            String[] keyVal = header.split(": ");
-            this.header.addHeader(keyVal[0], keyVal[1]);
-        }
-    }
-
-    private void parseRequestBody(BufferedReader br) throws IOException {
-        String length = header.getHeader("Content-Length");
-        if (length == null) {
-            return;
-        }
-
-        int contentLength = Integer.parseInt(length);
-        char[] body = new char[contentLength];
-        br.read(body, 0, contentLength);
-        String bodyString = URLDecoder.decode(new String(body),"UTF-8");
-
-        String[] bodyStringArray = bodyString.split("\\&");
-        Arrays.stream(bodyStringArray)
-                .forEach(keyVal -> {
-                    String[] keyValSplit = keyVal.split("=");
-                    requestParam.put(keyValSplit[0], keyValSplit[1]);
-                });
-        System.out.println(requestParam);
-    }
-    @Override
-    public String toString() {
-        return requestParam + "\n" +
-                path + "\n" +
-                httpMethod +
-                header;
-    }
-
 }
---
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequestBody.java
@@ -0,0 +1,50 @@
+package org.apache.coyote.request;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequestBody {
+
+    private String requestBody;
+
+    public HttpRequestBody() {
+        requestBody = "";
+    }
+
+    private HttpRequestBody(String requestBody) {
+        this.requestBody = requestBody;
+    }
+
+    public static HttpRequestBody parseBody(BufferedReader br, Object length) throws IOException {
+        if (length == null) {
+            return new HttpRequestBody();
+        }
+
+        int contentLength = Integer.parseInt((String) length);
+        char[] body = new char[contentLength];
+        br.read(body, 0, contentLength);
+        String bodyString = URLDecoder.decode(new String(body), "UTF-8");
+
+        return new HttpRequestBody(bodyString);
+    }
+
+    @Override
+    public String toString() {
+        return requestBody.toString();
+    }
+
+    public Map<String, String> getRequestBodyMap() {
+        Map<String, String> bodyMap = new HashMap<>();
+        String[] bodyStringArray = requestBody.split("\\&");
+        Arrays.stream(bodyStringArray)
+                .forEach(keyVal -> {
+                    String[] keyValSplit = keyVal.split("=");
+                    bodyMap.put(keyValSplit[0], keyValSplit[1]);
+                });
+        return bodyMap;
+    }
+}
