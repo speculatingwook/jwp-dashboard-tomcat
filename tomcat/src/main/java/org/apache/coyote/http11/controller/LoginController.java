9a117a48507b21ccd66f9b7145ada57a22780c90
jution113 <jution113@gmail.com>
2023-11-21 17:17:49 +0900

    Refactor: modify core logic for routing in LoginController

--- tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
+++ tomcat/src/main/java/org/apache/coyote/http11/controller/LoginController.java
@@ -2,20 +2,19 @@ package org.apache.coyote.http11.controller;
 
 import org.apache.coyote.http11.Service.LoginService;
 import org.apache.coyote.http11.request.HttpRequest;
-import org.apache.coyote.http11.response.ContentType;
-import org.apache.coyote.http11.response.HttpMethod;
-import org.apache.coyote.http11.response.HttpResponse;
-import org.apache.coyote.http11.response.HttpStatusCode;
+import org.apache.coyote.http11.request.HttpRequestBody;
+import org.apache.coyote.http11.request.HttpRequestHeader;
+import org.apache.coyote.http11.response.*;
 import org.apache.coyote.http11.session.Cookie;
 import org.apache.coyote.http11.session.Session;
-import org.apache.coyote.http11.session.SessionUtil;
+import org.apache.coyote.http11.session.SessionManager;
+import org.apache.coyote.http11.util.ParseUtil;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -23,93 +22,83 @@ public class LoginController implements Controller{
     private static final String INDEX_PAGE_URL = "index.html";
     private static final String NOT_FOUND_PAGE_URL = "static/404.html";
     private static final String UNAUTHORIZED_PAGE_URL = "401.html";
-    private static final String LOGIN__PAGE_URL = "static/login.html";
+    private static final String LOGIN_PAGE_URL = "static/login.html";
     private LoginService loginService = new LoginService();
-    @Override
-    public HttpResponse handleRequest(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException {
-        String requestUri = httpRequest.getPath();
-        String httpMethod = httpRequest.getMethod();
+    private Router router;
 
-
-        if(httpMethod.equals(HttpMethod.GET.getMethod())) {
-            Cookie cookie = httpRequest.getCookie();
-            String jSessionId = cookie.getValue("JSESSIONID");
-            Optional<Session> session = SessionUtil.findSession(jSessionId);
-            if (session.isPresent()) {
-                return new HttpResponse()
-                        .statusCode(HttpStatusCode.FOUND.getCode())
-                        .statusMessage(HttpStatusCode.FOUND.getMessage())
-                        .addHeader("Location", INDEX_PAGE_URL)
-                        .build();
-            }
-
-            URL resource = getClass()
-                    .getClassLoader()
-                    .getResource(LOGIN__PAGE_URL);
-            File file = new File(resource.getFile());
-
-            String responseBody = new String(Files.readAllBytes(file.toPath()));
-
-            return httpResponse = new HttpResponse()
-                    .statusCode(HttpStatusCode.OK.getCode())
-                    .statusMessage(HttpStatusCode.OK.getMessage())
-                    .addHeader("Content-Type", ContentType.findContentTypeFromUri(requestUri).getContentType())
-                    .addHeader("Content-Length", String.valueOf(responseBody.getBytes().length))
-                    .body(responseBody)
-                    .build();
-
-        } else if(httpMethod.equals(HttpMethod.POST.getMethod())) {
-            String requestBody = httpRequest.getBody();
-            Map<String, String> queryParameters = parseQueryParameters(requestBody);
-            String account = queryParameters.get("account");
-            String password = queryParameters.get("password");
-
-            if(loginService.login(account, password)) {
-                String jSessionId = SessionUtil.generateJSessionId();
-                Session session = Session.createSession(jSessionId);
-                SessionUtil.add(session);
-
-                httpResponse = new HttpResponse()
-                        .statusCode(HttpStatusCode.FOUND.getCode())
-                        .statusMessage(HttpStatusCode.FOUND.getMessage())
-                        .addHeader("Location", INDEX_PAGE_URL)
-                        .build();
-
-                httpResponse.addCookie("JSESSIONID", jSessionId);
-
-                return httpResponse;
-            }
-            return httpResponse = new HttpResponse()
-                    .statusCode(HttpStatusCode.UNAUTHORIZED.getCode())
-                    .statusMessage(HttpStatusCode.UNAUTHORIZED.getMessage())
-                    .addHeader("Location", UNAUTHORIZED_PAGE_URL)
-                    .build();
-        }
-
-        return httpResponse = new HttpResponse()
-                        .statusCode(HttpStatusCode.NOT_FOUND.getCode())
-                        .statusMessage(HttpStatusCode.NOT_FOUND.getMessage())
-                        .addHeader("Location", NOT_FOUND_PAGE_URL)
-                        .build();
+    public LoginController() {
+        router = new Router();
+        router.addHandler(HttpMethod.GET, "/login", this::handleGetLogin);
+        router.addHandler(HttpMethod.POST, "/login", this::handlePostLogin);
     }
 
-    public Map<String, String> parseQueryParameters(String query) throws UnsupportedEncodingException {
-        query = query.replace("\r\n", "");
-        String[] params = query.split("&");
+    @Override
+    public HttpResponse handleRequest(HttpRequest httpRequest) throws IOException {
+        return router.mappingEndpointHandler(httpRequest);
+    }
 
-        Map<String, String> queryParameters = new HashMap<>();
+    public HttpResponse handleGetLogin(HttpRequest httpRequest) throws IOException {
+        HttpRequestHeader httpRequestHeader = httpRequest.getHttpRequestHeader();
+        Optional<Cookie> optionalCookie = httpRequestHeader.getCookie();
 
-        for (String param : params) {
-            String[] keyValue = param.split("=");
-
-            if (keyValue.length == 2) {
-                String key = java.net.URLDecoder.decode(keyValue[0], "UTF-8");
-                String value = java.net.URLDecoder.decode(keyValue[1], "UTF-8");
-                queryParameters.put(key, value);
+        if(optionalCookie.isPresent()) {
+            Cookie cookie = optionalCookie.get();
+            String jSessionId = cookie.getJSessionId();
+            if(jSessionId != null) {
+                Optional<Session> optionalSession = SessionManager.findSession(jSessionId);
+                if(optionalSession.isPresent()) {
+                    return HttpResponse.builder()
+                            .statusCode(HttpStatusCode.FOUND.getCode())
+                            .statusMessage(HttpStatusCode.FOUND.getMessage())
+                            .addHeader("Location", INDEX_PAGE_URL)
+                            .build();
+                }
             }
         }
-        return queryParameters;
+
+        URL resource = getClass()
+                .getClassLoader()
+                .getResource(LOGIN_PAGE_URL);
+        File file = new File(resource.getFile());
+
+        String requestUri = httpRequestHeader.getPath();
+        String responseBody = new String(Files.readAllBytes(file.toPath()));
+
+        return HttpResponse.builder()
+                .statusCode(HttpStatusCode.OK.getCode())
+                .statusMessage(HttpStatusCode.OK.getMessage())
+                .addHeader("Content-Type", ContentType.findContentTypeFromUri(requestUri).getContentType())
+                .addHeader("Content-Length", String.valueOf(responseBody.getBytes().length))
+                .body(responseBody)
+                .build();
+    }
+
+    public HttpResponse handlePostLogin(HttpRequest httpRequest) throws UnsupportedEncodingException {
+        HttpRequestBody httpRequestBody = httpRequest.getHttpRequestBody();
+
+        String requestBody = httpRequestBody.getBody();
+        Map<String, String> queryParameters = ParseUtil.parseQueryParameters(requestBody);
+        String account = queryParameters.get("account");
+        String password = queryParameters.get("password");
+
+        if(loginService.login(account, password)) {
+            String jSessionId = SessionManager.generateJSessionId();
+            Session session = Session.createSession(jSessionId);
+            SessionManager.add(session);
+            Cookie cookie = new Cookie();
+
+            return HttpResponse.builder()
+                    .statusCode(HttpStatusCode.FOUND.getCode())
+                    .statusMessage(HttpStatusCode.FOUND.getMessage())
+                    .addHeader("Location", INDEX_PAGE_URL)
+                    .addCookie("JSESSIONID", jSessionId)
+                    .build();
+        }
+
+        return HttpResponse.builder()
+                .statusCode(HttpStatusCode.UNAUTHORIZED.getCode())
+                .statusMessage(HttpStatusCode.UNAUTHORIZED.getMessage())
+                .addHeader("Location", UNAUTHORIZED_PAGE_URL)
+                .build();
     }
 }
-
-
