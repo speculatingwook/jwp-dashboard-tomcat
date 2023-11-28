9696ace06cc71bb437a8a65705844ed6a5f1af2c
Han Ju Kim <hjkim2902@gmail.com>
2023-11-28 14:56:34 +1100

    feat : Add Cookie

--- tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
+++ tomcat/src/main/java/org/apache/coyote/request/HttpRequest.java
@@ -1,6 +1,9 @@
 package org.apache.coyote.request;
 
+import nextstep.util.Constant;
+import org.apache.coyote.session.Cookie;
 import org.apache.coyote.session.Session;
+import org.apache.coyote.session.SessionManager;
 
 import javax.naming.directory.NoSuchAttributeException;
 import java.io.BufferedReader;
@@ -10,25 +13,28 @@ public class HttpRequest {
     private HttpRequestBody requestBody;
     private HttpRequestLine httpRequestLine;
     private HttpRequestHeader header;
-
     private RequestParam requestParam;
     private Session session;
+    private Cookie cookie;
 
 
     public static HttpRequest parse(BufferedReader br) throws NoSuchAttributeException, IOException {
         HttpRequestLine requestLine = HttpRequestLine.parse(br);
         HttpRequestHeader header = HttpRequestHeader.parse(br);
-        HttpRequestBody body = HttpRequestBody.parseBody(br, header.getHeader("Content-Length"));
-        RequestParam params = RequestParam.from(requestLine,body);
-
-        return new HttpRequest(requestLine, header, body, params);
+        HttpRequestBody body = HttpRequestBody.parseBody(br, header.getHeader(Constant.CONTENT_LENGTH));
+        RequestParam params = RequestParam.from(requestLine, body);
+        Cookie httpCookie = Cookie.parseCookie(header.getHeader(Constant.COOKIE));
+        Session httpSession = SessionManager.getSession(httpCookie.getCookie(Constant.JSEESIONID));
+        return new HttpRequest(requestLine, header, body, params, httpSession, httpCookie);
     }
 
-    private HttpRequest(HttpRequestLine httpRequestLine, HttpRequestHeader header, HttpRequestBody requestBody, RequestParam param) {
+    private HttpRequest(HttpRequestLine httpRequestLine, HttpRequestHeader header, HttpRequestBody requestBody, RequestParam param, Session session, Cookie cookie) {
         this.httpRequestLine = httpRequestLine;
         this.header = header;
         this.requestBody = requestBody;
         this.requestParam = param;
+        this.session = session;
+        this.cookie = cookie;
     }
 
     public String getHttpMethod() {
@@ -41,14 +47,14 @@ public class HttpRequest {
 
     public Session getSession() {
         if (this.session == null) {
-            this.session = new Session();
+            this.session = SessionManager.createSession();
         }
         return session;
     }
 
     public Session getSession(boolean create) {
         if (create && this.session == null) {
-            this.session = new Session();
+            this.session = SessionManager.createSession();
         }
         return session;
     }
@@ -61,7 +67,7 @@ public class HttpRequest {
         return requestParam.getParameter(key);
     }
 
-    public String getCookie() {
-        return header.getHeader("Cookie");
+    public String getCookie(String key) {
+        return cookie.getCookie(key);
     }
 }
