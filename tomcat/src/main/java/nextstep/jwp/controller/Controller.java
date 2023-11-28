e9a7daabe21d6f4cf296d0610163947d324a67be
Han Ju Kim <hjkim2902@gmail.com>
2023-11-28 20:10:28 +1100

    refactor : Controller.execute() 반환값 수정

--- tomcat/src/main/java/nextstep/jwp/controller/Controller.java
+++ tomcat/src/main/java/nextstep/jwp/controller/Controller.java
@@ -6,5 +6,5 @@ import org.apache.coyote.response.HttpResponse;
 import java.io.IOException;
 
 public interface Controller {
-    HttpResponse execute(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException;
+    void execute(HttpRequest httpRequest, HttpResponse httpResponse) throws IOException;
 }
