b1a525b9b6499dce135a2e819300bc8b5afc7324
Han Ju Kim <hjkim2902@gmail.com>
2023-11-28 14:56:02 +1100

    feat : RequestMapping Get, Post 분리

--- tomcat/src/main/java/nextstep/jwp/RequestMapping.java
+++ tomcat/src/main/java/nextstep/jwp/RequestMapping.java
@@ -2,9 +2,11 @@ package nextstep.jwp;
 
 import nextstep.jwp.controller.CSSController;
 import nextstep.jwp.controller.Controller;
+import nextstep.jwp.controller.ExceptionController;
 import nextstep.jwp.controller.IndexController;
 import nextstep.jwp.controller.JSController;
 import nextstep.jwp.controller.LoginController;
+import nextstep.jwp.controller.LoginFormController;
 import nextstep.jwp.controller.RegisterController;
 import nextstep.jwp.controller.RegisterFormController;
 
@@ -23,7 +25,8 @@ public class RequestMapping {
     }
 
     private void initPostHandler() {
-        postHandlers.put("/register",new RegisterFormController());
+        postHandlers.put("/register", new RegisterFormController());
+        postHandlers.put("/login", new LoginFormController());
     }
 
     private void initGetHandler() {
@@ -37,6 +40,10 @@ public class RequestMapping {
     }
 
     public Controller getController(String method, String url){
+        System.out.println();
+        System.out.println("method = " + method);
+        System.out.println("url = " + url);
+        System.out.println();
         if(url.contains(".css")){
             return getHandlers.get("/css");
         }
@@ -50,6 +57,6 @@ public class RequestMapping {
         if(method.trim().equals("POST")){
             return postHandlers.get(url);
         }
-        return getHandlers.get(url);
+        return new ExceptionController();
     }
 }
